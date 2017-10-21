d927d97679aaa5d95b194a8e24caf5ac089f3662
Hidetake Iwata <int128@gmail.com>
2017-10-21 13:57:15 +0900

    Fix session header did not appear if controller returns stream

--- acceptance-test/src/main/java/example/FixtureApp.java
+++ acceptance-test/src/main/java/example/FixtureApp.java
@@ -1,20 +1,10 @@
 package example;
 
-import org.springframework.boot.SpringApplication;
+import org.hidetake.spring.session.appengine.EnableMemcacheHttpSession;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
-@EnableSpringHttpSession
+@EnableMemcacheHttpSession
 public class FixtureApp extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(FixtureApp.class);
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(FixtureApp.class, args);
-    }
 }
--- acceptance-test/src/main/java/example/FixtureController.java
+++ acceptance-test/src/main/java/example/FixtureController.java
@@ -5,20 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class FixtureController {
     private final Foo foo;
 
+    @GetMapping("/sid")
+    @ResponseBody
+    String sid(HttpSession session) {
+        return session.getId();
+    }
+
     @GetMapping("/foo")
     @ResponseBody
-    int index() {
-        return foo.getCount();
+    String index() {
+        return String.valueOf(foo.getCount());
     }
 
     @GetMapping("/foo/increment")
     @ResponseBody
-    int increment() {
-        return foo.increment();
+    String increment() {
+        return String.valueOf(foo.increment());
     }
 }
